43e857feedb03d25d22fb0d2a5ec41fa0194df01
kasiashabanova <58844522+kasiashabanova@users.noreply.github.com>
2020-01-07 15:08:56 +0100

    Add files via upload

---
+++ SumaCyfr1.cpp
@@ -0,0 +1,48 @@
+
+/// suma cyfr liczby ca³kowitej
+
+/// autor:Ekaterina Shabanova
+
+/// Data: 2020-01-07
+
+
+
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int number, r;
+
+    int sum = 0;
+
+    cin >> number;
+
+
+
+    while(number>0)
+
+    {
+
+        r = number % 10;
+
+        sum += r;
+
+        number /= 10;
+
+    }
+
+    cout << sum;
+
+    return 0;
+
+
+
+}
