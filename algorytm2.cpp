060aea50bb6dbace0d5db7e9e66be3ab7e31c0d1
kasiashabanova <58844522+kasiashabanova@users.noreply.github.com>
2020-01-07 15:17:28 +0100

    Add files via upload

---
+++ algorytm2.cpp
@@ -0,0 +1,40 @@
+
+/// zamiana liczby dziesiêtnej na dwójkkow¹
+/// autor:Ekaterina Shabanova
+/// Data: 2020-01-07
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+
+{
+
+    int dec;
+
+string bin = "";
+
+    cin >> dec;
+
+
+
+    while(dec>0)
+
+    {
+        if (dec %2 == 0)
+            bin += "0";
+            else
+            bin += 1;
+            dec /=  2;
+    }
+    reverse (bin.begin(), bin.end());
+
+    cout << bin;
+
+    return 0;
+
+
+
+}
